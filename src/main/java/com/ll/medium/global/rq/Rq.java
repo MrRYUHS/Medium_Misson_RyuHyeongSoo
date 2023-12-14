f83d3184c2411ebfe764b9ebf65cde577004e814
HyeongSoo <rhsrsy123@gmail.com>
2023-12-14 15:28:41 +0900

    1단계

--- src/main/java/com/ll/medium/global/rq/Rq.java
+++ src/main/java/com/ll/medium/global/rq/Rq.java
@@ -1,14 +1,20 @@
 package com.ll.medium.global.rq;
 
 import com.ll.medium.global.rsdata.RsData;
+import com.ll.medium.standard.util.ut.Ut;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.annotation.RequestScope;
 
 import java.net.URLEncoder;
 import java.nio.charset.StandardCharsets;
+import java.util.Optional;
 
 @Component
 @RequestScope
@@ -43,5 +49,48 @@ public class Rq {
         if (rs.isFail()) return historyBack(rs.getMsg());
 
         return redirect(path, rs.getMsg());
+    }
+
+    public User getUser() {
+        return Optional.ofNullable(SecurityContextHolder.getContext())
+                .map(SecurityContext::getAuthentication)
+                .map(Authentication::getPrincipal)
+                .filter(it -> it instanceof User)
+                .map(it -> (User) it)
+                .orElse(null);
+    }
+
+    public boolean isLogin() {
+        return getUser() != null;
+    }
+
+    public boolean isLogout() {
+        return !isLogin();
+    }
+
+    public boolean isAdmin() {
+        if(isLogout()) return false;
+
+        return getUser()
+                .getAuthorities()
+                .stream()
+                .anyMatch(it ->
+                        it.getAuthority().equals("ROLE_ADMIN"));
+    }
+
+    public void setAttribute(String key, Object value) {
+        request.setAttribute(key, value);
+    }
+
+    public String getCurrentQueryStringWithoutParam(String paramName) {
+        String queryString = request.getQueryString();
+
+        if (queryString == null) {
+            return "";
+        }
+
+        queryString = Ut.url.deleteQueryParam(queryString, paramName);
+
+        return queryString;
     }
 }
